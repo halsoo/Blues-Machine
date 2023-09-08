8ec3dcb61500281578f13b35b983f1c990c59912
halsoo <noizyufo@gmail.com>
2023-09-08 09:39:38 +0900

    add: wandb setup and scripts for training

--- train.py
+++ train.py
@@ -1,40 +1,121 @@
-import time
+# config
+import sys, getopt
+from omegaconf import OmegaConf
 
+# torch
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.optim as optim
 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-
+# models
 from model import Encoder, Decoder, Seq2Seq
 from dataset import BluesPairSet, make_collate_fn
 from trainer import Trainer
 
+# wandb
+import wandb
 
-dataset = BluesPairSet()
+def getConfs(argv):
+  args = argv[1:]
+  
+  try:
+    opt_list, _ = getopt.getopt(args, 'f:n:p:e:')
+    
+  except getopt.GetoptError:
+    print('somethings gone wrong')
+    return None
 
-DEVICE = 'cuda'
+  opt_dict = {}
+  
+  for opt in opt_list:
+    name, value = opt 
+    name = name.replace('-', '')
+    
+    if value:
+      opt_dict[name] = value
+    else:
+      opt_dict[name] = name
+    
+  conf = OmegaConf.load(opt_dict['f'])
+  
+  if opt_dict.get('e'):
+    num_epoch_arg = int(opt_dict['e'])
+    conf.num_epoch = num_epoch_arg
+    
+  if opt_dict.get('n'):
+    name_arg = opt_dict['n']
+    conf.run_name = name_arg
+    
+  if opt_dict.get('p'):
+    project_arg = opt_dict['p']
+    conf.project_name = project_arg
+  
+  return conf
 
-EMB_LEN = dataset.vocab_size
-EMB_DIM = 64
-HID_DIM = 256
-NUM_LAYER = 3
-PAD_IDX = dataset.token2idx['<pad>']
+def train(argv):
+  
+  conf = getConfs(argv)
+  
+  dataset = BluesPairSet()
 
-encoder = Encoder(EMB_LEN, EMB_DIM, HID_DIM, num_layers=NUM_LAYER, device=DEVICE) # input size(embed len), embed size, hidden size
-decoder = Decoder(EMB_LEN, EMB_DIM, HID_DIM, num_layers=NUM_LAYER, device=DEVICE) # output size(embed len), embed size, hidden size
+  # Device conf
+  DEVICE = conf.device
+  
+  # RNN conf
+  EMB_LEN = dataset.vocab_size
+  EMB_DIM = conf.rnn.emb_dim
+  HID_DIM = conf.rnn.hid_dim
+  NUM_LAYER = conf.rnn.num_layer
+  
+  # Data conf
+  PAD_IDX = dataset.token2idx[conf.data.pad_token]
+  TRAIN_SPLIT = conf.data.train.split
+  TRAIN_BATCH_SIZE = conf.data.train.batch_size
+  VALID_SPLIT = conf.data.valid.split
+  VALID_BATCH_SIZE = conf.data.valid.batch_size
+  
+  # Train conf
+  LR = conf.lr
+  NUM_EPOCH = conf.num_epoch
+  
+  hyperparams = { 
+    'rnn_emb_len': EMB_LEN,
+    'rnn_emb_dim': EMB_DIM, 
+    'rnn_hid_dim': HID_DIM,
+    'rnn_num_layer': NUM_LAYER, 
+    'train_set_len': TRAIN_SPLIT, 
+    'train_batch_size': TRAIN_BATCH_SIZE, 
+    'valid_set_len': VALID_SPLIT, 
+    'valid_batch_size': VALID_BATCH_SIZE,
+    'epoch': conf.num_epoch,
+    'learning_rate': LR
+  }
+  
+  wandb_run = wandb.init(
+    project=conf.project_name,
+    name=conf.run_name,
+    config=hyperparams
+  )
 
-model = Seq2Seq(encoder, decoder, device=DEVICE)
+  encoder = Encoder(EMB_LEN, EMB_DIM, HID_DIM, num_layers=NUM_LAYER, device=DEVICE) # input size(embed len), embed size, hidden size
+  decoder = Decoder(EMB_LEN, EMB_DIM, HID_DIM, num_layers=NUM_LAYER, device=DEVICE) # output size(embed len), embed size, hidden size
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-loss_fn = nn.CrossEntropyLoss(ignore_index = PAD_IDX)
+  model = Seq2Seq(encoder, decoder, device=DEVICE)
 
-collate = make_collate_fn(DEVICE)
+  optimizer = optim.Adam(model.parameters(), lr=LR)
+  loss_fn = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+  
+  collate_fn = make_collate_fn(DEVICE)
 
-trainset, validset = torch.utils.data.random_split(dataset, [42, 10])
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, collate_fn=collate)
-valid_loader = torch.utils.data.DataLoader(validset, batch_size=10, shuffle=False, collate_fn=collate)
+  trainset, validset = torch.utils.data.random_split(dataset, [TRAIN_SPLIT, VALID_SPLIT])
+  train_loader = torch.utils.data.DataLoader(trainset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
+  valid_loader = torch.utils.data.DataLoader(validset, batch_size=VALID_BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
 
-trainer = Trainer(model, optimizer, loss_fn, train_loader, valid_loader, device=DEVICE)
+  trainer = Trainer(model, optimizer, loss_fn, train_loader, valid_loader, device=DEVICE, wandb=wandb_run)
+  
+  trainer.train_by_num_epoch(NUM_EPOCH, do_validate=True)
+
+if __name__ == '__main__':
+  train(sys.argv)
+  
--- trainer.py
+++ trainer.py
@@ -6,128 +6,127 @@ from tqdm import tqdm
 import torch
 
 class Trainer:
-    def __init__(self, model, optimizer, loss_fn, train_loader, valid_loader, device='cpu'):
-        self.model = model
-        self.optimizer = optimizer
-        self.loss_fn = loss_fn
-        self.train_loader = train_loader
-        self.valid_loader = valid_loader
-        
-        self.device = device
-        self.model.to(self.device)
-        self.loss_fn.to(self.device)
-        
-        self.best_valid_accuracy = 0
-        
-        self.training_loss = []
-        self.validation_loss = []
-        self.validation_acc = []
+  def __init__(self, model, optimizer, loss_fn, train_loader, valid_loader, device='cpu', wandb=None):
+    self.model = model
+    self.optimizer = optimizer
+    self.loss_fn = loss_fn
+    self.train_loader = train_loader
+    self.valid_loader = valid_loader
+    
+    self.device = device
+    self.model.to(self.device)
+    self.loss_fn.to(self.device)
+    
+    self.best_valid_accuracy = 0
+    
+    self.training_loss = []
+    self.validation_loss = []
+    self.validation_acc = []
+    
+    self.wandb = wandb
 
-    def save_model(self, path='models/', surfix=''):
-        model_default_name = 'blues_call_and_res_model-'
-        torch.save({'model':self.model.state_dict(), 'optim':self.optimizer.state_dict()}, f'{path}{model_default_name}{int(time.time())}{surfix}.pt')
+  def save_model(self, path='models/', surfix=''):
+    model_default_name = 'blues_call_and_res_model'
+    torch.save({'model':self.model.state_dict(), 'optim':self.optimizer.state_dict()}, f'{path}{model_default_name}{surfix}.pt')
         
-    def _get_accuracy(self, pred, target_sqz):
-        # pred: [batch size * pred len, num vocab]
-        # target_sqz: [batch size * target len]
+  def _get_accuracy(self, pred, target_sqz):
+    # pred: [batch size * pred len, num vocab]
+    # target_sqz: [batch size * target len]
         
-        is_correct = pred.argmax(dim=-1).to(self.device) == target_sqz
-        return (is_correct.sum() / target_sqz.reshape(-1,).shape[-1]).item()
+    is_correct = pred.argmax(dim=-1).to(self.device) == target_sqz
+    return (is_correct.sum() / target_sqz.reshape(-1,).shape[-1]).item()
         
-    def train_by_num_epoch(self, num_epochs, tfr=0.5, do_validate=False): # tfr: teacher forcing ratio
-        for epoch in tqdm(range(num_epochs)):
-            self.model.train()
+  def train_by_num_epoch(self, num_epochs, do_validate=False): # tfr: teacher forcing ratio
+    for epoch in tqdm(range(num_epochs)):
+      self.model.train()
             
-            for batch in self.train_loader:
-                loss_value = self._train_by_single_batch(batch, tfr=tfr)
-                self.training_loss.append(loss_value)
+      for batch in self.train_loader:
+        loss_value = self._train_by_single_batch(batch)
+        self.training_loss.append(loss_value)
+        
+        self.wandb.log({
+          'loss_train': loss_value
+        })
             
-            if do_validate:
-                self.model.eval()
-                
-                validation_loss, validation_acc = self.validate()
-                self.validation_loss.append(validation_loss)
-                self.validation_acc.append(validation_acc)
-                
-                if validation_acc > self.best_valid_accuracy:
-                    print(f"Saving the model with best validation accuracy: Epoch {epoch+1}, Acc: {validation_acc:.4f} ")
-                    self.save_model()
-                # else:
-                #     self.save_model()
-                    
-                self.best_valid_accuracy = max(validation_acc, self.best_valid_accuracy)
+        if do_validate:
+          self.model.eval()
+          validation_loss, validation_acc = self.validate()
+          self.validation_loss.append(validation_loss)
+          self.validation_acc.append(validation_acc)
+          
+          if self.wandb:
+            self.wandb.log({
+              'loss_valid': validation_loss,
+              'acc_valid': validation_acc,
+            })
+          
+          if validation_acc > self.best_valid_accuracy:
+            print(f"Saving the model with best validation accuracy: Epoch {epoch+1}, Acc: {validation_acc:.4f} ")
+            self.save_model(surfix='-best_acc')
+              
+          self.best_valid_accuracy = max(validation_acc, self.best_valid_accuracy)
+    
+    self.save_model()
+    
+    self.wandb.finish()
 
         
-    def _train_by_single_batch(self, batch, tfr=0.5):
-        self.model.train()
+  def _train_by_single_batch(self, batch):
+    self.model.train()
 
+    call, res = batch
+    call.to(self.device)
+    res.to(self.device)
+    output = self.model(call, res) # (batch size, target len, target vocab size)
+    
+    vocab_size = output.shape[-1]
+    
+    res = res[:, 1:].contiguous().view(-1)
+    output = output.view(-1, vocab_size)
+    
+    loss = self.loss_fn(output, res)
+    
+    self.optimizer.zero_grad()
+    
+    loss.backward()
+    
+    self.optimizer.step()
+    
+    return loss.item()
+
+
+  def validate(self, external_loader=None):
+    if external_loader and isinstance(external_loader, DataLoader):
+      loader = external_loader
+      print('An arbitrary loader is used instead of Validation loader')
+    else:
+      loader = self.valid_loader
+    
+    self.model.eval()
+    
+    with torch.no_grad():
+      valid_loss = 0
+      valid_acc = 0
+      batch_cnt = 0
+        
+      for batch in loader:
         call, res = batch
-        call.to(self.device)
-        res.to(self.device)
-        output = self.model(call, res, teacher_forcing_ratio=tfr) # (batch size, target len, target vocab size)
-        
+            
+        output = self.model(call, res) # (batch size, target len, target vocab size)
+            
         vocab_size = output.shape[-1]
-        
+            
         res = res[:, 1:].contiguous().view(-1)
         output = output.view(-1, vocab_size)
-        
-        loss = self.loss_fn(output, res)
-        
-        self.optimizer.zero_grad()
-        
-        loss.backward()
-        
-        self.optimizer.step()
-        
-        return loss.item()
-
-
-    def validate(self, external_loader=None):
-        
-        if external_loader and isinstance(external_loader, DataLoader):
-            loader = external_loader
-            print('An arbitrary loader is used instead of Validation loader')
-        else:
-            loader = self.valid_loader
-        
-        self.model.eval()
-        
-        with torch.no_grad():
-            valid_loss = 0
-            valid_acc = 0
-            batch_cnt = 0
             
-            for batch in loader:
-                call, res = batch
-                
-                output = self.model(call, res, teacher_forcing_ratio=1.0) # (batch size, target len, target vocab size)
-                
-                vocab_size = output.shape[-1]
-                
-                res = res[:, 1:].contiguous().view(-1)
-                output = output.view(-1, vocab_size)
-                
-                loss = self.loss_fn(output, res)
-                
-                valid_loss += loss.item()
-                valid_acc += self._get_accuracy(output, res)
-                
-                batch_cnt += 1
-                
-            valid_loss /= batch_cnt
-            valid_acc /= batch_cnt
-                
-            return valid_loss, valid_acc
-
-
-def load_loss(date, num):
-    try:
-        with open(f"models/blues_call_and_res_model-loss-{date}-{num}.p", 'rb') as f: 
-            return pickle.load(f)
-    except FileNotFoundError:
-        return {}
-
-
-def save_loss(date, num, loss_obj): # loss obj = { train_loss, valid_loss }
-    with open(f"models/blues_call_and_res_model-loss-{date}-{num}.p", 'wb') as f:
-        pickle.dump(loss_obj, f)
+        loss = self.loss_fn(output, res)
+            
+        valid_loss += loss.item()
+        valid_acc += self._get_accuracy(output, res)
+            
+        batch_cnt += 1
+            
+      valid_loss /= batch_cnt
+      valid_acc /= batch_cnt
+            
+      return valid_loss, valid_acc
